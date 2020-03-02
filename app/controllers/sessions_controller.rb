fcd9a0c3f4f4cce95ec08cd4537618a2e61919c1
Michael Deitcher <michaeldeitcher@gmail.com>
2020-03-02 12:43:34 -0700

    handling failed login properly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < Devise::SessionsController
   def create
-    self.resource = warden.authenticate!(auth_options)
-    sign_in(resource_name, resource)
-    json = {
-      data: {
-        id: resource.id, type: 'User',
-        attributes: resource.attributes.slice('username', 'email', 'authentication_token')
-      }
-    }.to_json
-    render json: json, status: 201
+    self.resource = warden.authenticate(auth_options)
+    if resource
+      sign_in(resource_name, resource)
+      json = {
+        data: {
+          id: resource.id, type: 'User',
+          attributes: resource.attributes.slice('username', 'email', 'authentication_token')
+        }
+      }.to_json
+      render json: json, status: 201
+    else
+      render_unauthorized
+    end
   end
 end
