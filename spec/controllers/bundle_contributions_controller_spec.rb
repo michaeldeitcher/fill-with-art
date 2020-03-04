62d5fa6822bbbc2e558e608e14c5c27ebe0b9be2
Michael Deitcher <michaeldeitcher@gmail.com>
2020-03-04 14:25:33 -0700

    bundle contribution spec work

--- spec/controllers/bundle_contributions_controller_spec.rb
+++ spec/controllers/bundle_contributions_controller_spec.rb
@@ -6,19 +6,27 @@ RSpec.describe BundleContributionsController, type: :controller do
     @request.env['devise.mapping'] = Devise.mappings[:user]
   end
 
-  describe "POST #create" do
+  describe 'POST #create' do
     let(:bundle) { create(:bundle, creator: user, anonymous_token: '123456') }
     let(:params) {
       { bundle_contribution:
-        { bundle_id: bundle.friendly_id, anonymous_token: '123456' }
+        { bundle_id: bundle.friendly_id, text: 'hello', anonymous_token: '65321' }
       }
     }
     subject { post :create, params: params }
 
-    it "should create a bundle contribution" do
-      subject
-      p response.body
+    it 'should create a bundle contribution' do
+      expect{ subject }.to change { BundleContribution.count }.by(1)
+
       expect(response).to have_http_status 201
+    end
+
+    describe 'with missing params' do
+      it 'should respond with errors' do
+        post :create, params: {bundle_contribution: { bundle_id: bundle.friendly_id }}
+
+        expect(response).to have_http_status 422
+      end
     end
   end
 end
