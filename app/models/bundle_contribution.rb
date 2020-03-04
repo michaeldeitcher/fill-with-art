8c2b695018ee366513eac2915bb6743746ad2dfb
Michael Deitcher <michaeldeitcher@gmail.com>
2020-03-04 13:03:51 -0700

    bundle contribution model
    writing spec for validation and ordering
    implementation

--- app/models/bundle_contribution.rb
+++ app/models/bundle_contribution.rb
@@ -4,4 +4,33 @@ class BundleContribution < ApplicationRecord
   belongs_to :creator, class_name: 'User', optional: true
 
   validates :anonymous_token, presence: true
+  validate :verify_new_anonymous_token
+  validate :verify_text_or_image_present
+  before_validation :assure_contribution_order
+
+  def assure_contribution_order
+    self.contribution_order = bundle.bundle_contributions.count if bundle
+  end
+
+  def last_contribution
+    bundle.bundle_contributions.where(contribution_order: contribution_order - 1).limit(1).first
+  end
+
+  def verify_text_or_image_present
+    unless(text.present? || image.present?)
+      error = 'Text or image must be present'
+      errors.add(:image, error)
+      errors.add(:text, error)
+    end
+  end
+
+  def verify_new_anonymous_token
+    return unless bundle
+    self.bundle.bundle_contributions.each {|o| p o}
+    if (contribution_order == 0 &&
+          bundle.anonymous_token == anonymous_token) ||
+       (last_contribution.try(:anonymous_token) == anonymous_token)
+      errors.add(:anonymous_token, 'Must be different from the last contribution.')
+    end
+  end
 end
--- spec/models/bundle_contribution_spec.rb
+++ spec/models/bundle_contribution_spec.rb
@@ -1,14 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe BundleContribution, type: :model do
-  subject { build(:bundle_contribution, { bundle: build(:bundle) }) }
+  let(:first_user) { create(:user) }
+  let(:bundle) { create(:bundle) }
+  let(:first_contribution) { build(:bundle_contribution, bundle: bundle) }
 
-  it 'should be valid' do
-    expect(subject).to be_valid
+  context 'on first contribution' do
+    it 'should be valid' do
+      expect(first_contribution).to be_valid
+    end
+
+    it 'should require a bundle' do
+      first_contribution.bundle = nil
+      expect(first_contribution).to_not be_valid
+    end
+
+    it 'should require an image or text' do
+      first_contribution.text = nil
+      first_contribution.image = nil
+      expect(first_contribution).to_not be_valid
+    end
+
+    it 'should be assigned a contribution_order' do
+      expect(first_contribution).to be_valid
+      expect(first_contribution.contribution_order).to be(0)
+    end
+
+    it 'should require a new token' do
+      first_contribution.anonymous_token = nil
+      expect(first_contribution).to_not be_valid
+    end
+
+    it 'should require a different token than the bundle' do
+      first_contribution.anonymous_token = first_contribution.bundle.anonymous_token
+      expect(first_contribution).to_not be_valid
+    end
   end
 
-  it 'should require a bundle' do
-    subject.bundle = nil
-    expect(subject).to_not be_valid
+  context 'on second contribution' do
+    before { first_contribution.save! }
+    let(:second_contribution) { build(:bundle_contribution, bundle: bundle) }
+
+    it 'should be valid' do
+      expect(second_contribution).to be_valid
+    end
+
+    it 'should be assigned a contribution_order' do
+      expect(second_contribution).to be_valid
+      expect(second_contribution.contribution_order).to be(1)
+    end
+
+    it 'should require a different token than the first' do
+      second_contribution.anonymous_token = first_contribution.anonymous_token;
+
+      expect(second_contribution).to_not be_valid
+    end
   end
 end
