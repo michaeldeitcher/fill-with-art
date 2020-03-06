c100399a5002e6f0094c5aa63f18b8ebcb254ea2
Michael Deitcher <michaeldeitcher@gmail.com>
2020-03-06 00:01:48 -0700

    ws cors config - trying again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://ancient-plains-38653.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://www.bundlebundle.org', 'https://bundlebundle.org', /http:\/\/bundlebundle.*/ ]
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
